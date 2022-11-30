c1f3a413436b7cfe1602562531fd1556113a0cfa
Kourosh Arfania <arfania@usc.edu>
2022-11-30 03:17:49 +0000

    Final submission

--- README.md
+++ README.md
@@ -19,3 +19,15 @@
 (4) Enter `cd hw5_tests/wordle_tests/`, `make wordle_tests`, and `./wordle_tests` run more comprehensive tests
 
 (5) Run `valgrind --tool=memcheck --leak-check=yes ./wordle_tests` to check for memory loss/leaks
+
+### Final Submission Notes
+##### schedwork
+(1) Ensure you are in the `hw5` directory
+
+(2) Run `make schedwork-driver` to compile and link all files for the schedwork code
+
+(3) Enter `./schedwork-driver` in your terminal to run the basic tests provided in the Codio guide
+
+(4) Enter `cd hw5_tests/schedwork_tests/`, `make schedwork_tests`, and `./schedwork_tests` run more comprehensive tests
+
+(5) Run `valgrind --tool=memcheck --leak-check=yes ./schedwork_tests` to check for memory loss/leaks
--- schedwork.cpp
+++ schedwork.cpp
@@ -7,21 +7,131 @@
 #include <vector>
 #include <map>
 #include <algorithm>
-// add or remove necessary headers as you please
 #include "schedwork.h"
 
 using namespace std;
 
 // a constant that can be used to indicate an INVALID 
-// worker ID if that is useful to your implementation.
-// Feel free to not use or delete.
+// worker ID 
 static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
-// Add prototypes for any helper functions here
+// Prototypes for helper functions
+bool scheduleHelper(
+	 	const AvailabilityMatrix& avail,
+    const size_t dailyNeed,
+    const size_t maxShifts,
+    DailySchedule& sched,
+		size_t currentDay,
+		size_t currentWorkers
+);
 
+bool isValid(
+	 	const AvailabilityMatrix& avail,
+    const size_t dailyNeed,
+    const size_t maxShifts,
+    DailySchedule& sched,
+		size_t currentDay,
+		size_t currentWorkers,
+		Worker_T id
+);
 
-// Add your implementation of schedule() and other helper functions here
+// Implementations of functions
+
+/* 
+ * Function checking if the current
+ * worker is a valid choice given
+ * requirements set by the prompt
+ */
+
+bool isValid(
+	 	const AvailabilityMatrix& avail,
+    const size_t dailyNeed,
+    const size_t maxShifts,
+    DailySchedule& sched,
+		size_t currentDay,
+		size_t currentWorkers,
+		Worker_T id
+)
+{
+	size_t count = 0;
+	// Check how many times they have been scheduled thus far
+	for (size_t i = 0; i < sched.size(); ++i)
+	{
+		for (size_t j = 0; j < dailyNeed; ++j)
+		{
+			if (sched[i][j] == id)
+			{
+				count++;
+			}
+		}
+	}
+
+	// If the count is the wrong size, then return false
+	return (count <= maxShifts);
+}
+
+/* 
+ * Primary helper function,
+ * calling isValid() and 
+ * implementing backtracing
+ */
+bool scheduleHelper(
+	 	const AvailabilityMatrix& avail,
+    const size_t dailyNeed,
+    const size_t maxShifts,
+    DailySchedule& sched,
+		size_t currentDay,
+		size_t currentWorkers
+)
+
+// currentDay = row for sched
+// currentWorkers = col for sched
+{
+	// Base Case: we reached the end of the schedule
+	if (currentDay == sched.size())
+	{
+		return true;
+	}
+
+		// Trying to schedule each worker
+		for (size_t i = 0; i < avail[0].size(); ++i)
+		{
+			if (avail[currentDay][i]){
+				// If this worker is already scheduled for today, move on
+				if (find(sched[currentDay].begin(), sched[currentDay].end(), i) != sched[currentDay].end())
+				{
+					continue;
+				}
+
+				// Schedule worker and test if valid for this and following iteration
+				sched[currentDay][currentWorkers] = i;			
+				if (isValid(avail, dailyNeed, maxShifts, sched, currentDay, currentWorkers, i))
+				{
+					// If we are at the end of column, that means the next call must start on new row (day + 1)
+					if (currentWorkers == dailyNeed - 1)
+					{
+						if (scheduleHelper(avail, dailyNeed, maxShifts, sched, currentDay + 1, 0))
+						{
+							return true;
+						}
+					}
+					// If we still have space in this row, increment the column (worker + 1)
+					else
+					{
+						if (scheduleHelper(avail, dailyNeed, maxShifts, sched, currentDay, currentWorkers + 1))
+						{
+							return true;
+						}
+					}
+				}
+			}
+		}
+
+		// If not valid after trying all workers, backtrace
+		sched[currentDay][currentWorkers] = INVALID_ID;
+		return false;
+}
 
 bool schedule(
     const AvailabilityMatrix& avail,
@@ -34,10 +144,16 @@ bool schedule(
         return false;
     }
     sched.clear();
-    // Add your code below
 
+		// Initializing the sched matrix of size n x d
+		std::vector<Worker_T> init(dailyNeed, INVALID_ID);
+    for (size_t row = 0; row < avail.size(); ++row)
+    {
+				sched.push_back(init);
+    }
 
-
+    // Call helper and return it's value
+		return scheduleHelper(avail, dailyNeed, maxShifts, sched, 0, 0);
 
 }
 
